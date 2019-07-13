5c68b57a82bee6d4cb24555c77929a5d61f4d40c
Selen Gora <goraselen@gmail.com>
2019-07-13 23:40:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quotes = 'http://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+const quotes = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 const textContainer = document.querySelector('#text');
 const authorContainer = document.querySelector('#author');
 const tweetQuote = document.querySelector('#tweet-quote');
